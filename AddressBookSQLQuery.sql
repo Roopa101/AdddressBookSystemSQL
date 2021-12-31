106da561d23da82562e3416032e2cf5fb230cd4f
Roopa Jamadar <roopajamadar14@gmail.com>
2021-12-31 15:03:17 +0530

    [Roopa] Added: Inserted New Contacts To AddressBook

--- AddressBookSQLQuery.sql
+++ AddressBookSQLQuery.sql
@@ -18,4 +18,14 @@ Email varchar(35)
 );
 
 exec sp_help address_Book
-select * from address_Book 
+select * from address_Book
+
+
+
+insert into address_Book(Firstname,Lastname,address,City,State,Zip,PhoneNumber,Email) Values
+( 'Roopa', 'Jamadar', 'Khandal', 'Kalaburagi', 'Karnataka', '585304', '9901123984', 'roopajamadar123@bridgelabz.com' ),
+( 'Shradha', 'Jadhav', 'Pune', 'Pune', 'Maharashtra', '411001', '8934567523', 'shradha799@bridgelabz.com' ),
+( 'Vaishnavi', 'Vishwakarma', 'Bidar', 'Bidar', 'Karnataka', '565304', '8943026538', 'vaish756@bridgelabz.com' ),
+( 'Ganesh', 'Jamadar', 'kantekur', 'umerga', 'Maharashtra', '413606', '9038294056', 'prabhu999@bridgelabz.com' ),
+( 'Sireesha', 'Vemulapalli', 'GandhiNagar', 'Kurnool', 'AndhraPradesh', '562134', '9876543210', 'siri9153@bridgelabz.com' ),
+( 'NaveenChand', 'Moparthi', 'Chinaravuru', 'Tenali', 'AndhraPradesh', '568753', '8796054859', 'naveen799@bridgelabz.com' );
